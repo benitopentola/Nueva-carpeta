9f0ff734f97ce88e2f7a06e8a59b9af056a038fc
Javier <javiergraciafn@gmail.com>
2022-06-29 14:13:05 +0200

    commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i=1;
+		do{
+			Console.WriteLine("Hello, World!");
+			i++;
+		}while(i<99);
+
+		int z = 1;
+		while (z<10)
+		{
+			Console.WriteLine("Culo" + z);
+			z ++;
+		}
+	}
+}
